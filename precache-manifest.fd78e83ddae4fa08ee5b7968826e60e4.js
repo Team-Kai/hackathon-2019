bcabcd248d4f4a3cbc114fbdfdce7ecca74a1c4d
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-04 09:59:51 -0400

    Updates

---
+++ precache-manifest.fd78e83ddae4fa08ee5b7968826e60e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8e3a18c846fef49c7354f68ddc6bf2",
+    "url": "/hackathon-2019/index.html"
+  },
+  {
+    "revision": "628b4d6501e176cfa960",
+    "url": "/hackathon-2019/static/css/main.50dbc33a.chunk.css"
+  },
+  {
+    "revision": "185e26bf889173089d89",
+    "url": "/hackathon-2019/static/js/2.3fd8a9e5.chunk.js"
+  },
+  {
+    "revision": "628b4d6501e176cfa960",
+    "url": "/hackathon-2019/static/js/main.0d9ab81b.chunk.js"
+  },
+  {
+    "revision": "61027102c49f50e14a04",
+    "url": "/hackathon-2019/static/js/runtime-main.96642a7d.js"
+  }
+]);
