ced1d278c487800a37dee79671d00f60d053a6e2
Bobby Wang <bobby.p.wang@gmail.com>
2019-10-04 16:35:08 -0400

    Added styling to the home modal

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import React, { Component } from 'react'
-import { Route, Switch} from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 
-import './App.scss';
-import Home from './components/Home';
-import SearchResults from './components/SearchResults';
-import Listing from './components/Listing';
-import Navigation from './components/partials/Navigation';
+import "./App.scss";
+import Home from "./components/Home";
+import SearchResults from "./components/SearchResults";
+import Listing from "./components/Listing";
+import Navigation from "./components/partials/Navigation";
 
-import ModalForm from './components/partials/ModalForm';
-
+import ModalForm from "./components/partials/ModalForm";
 
 export default class App extends Component {
-
   render() {
-
     return (
       <div className="App">
-        
-   
- <video controls autoplay muted loop id="myVideo">
-   <source src="./assets/new-mexico--airbnb.mp4"
-   type="video/mp4"></source>
-   </video>
+        <video autoplay muted loop id="myVideo">
+          <source
+            src="./assets/new-mexico--airbnb.mp4"
+            type="video/mp4"
+          ></source>
+        </video>
 
-          <Navigation />
-          
+        <Navigation />
+
         <div className="container">
           <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/listing/:id' component={Listing} />
-            <Route path='/search' component={SearchResults} />
+            <Route exact path="/" component={Home} />
+            <Route path="/listing/:id" component={Listing} />
+            <Route path="/search" component={SearchResults} />
           </Switch>
         </div>
-     
       </div>
-    )
+    );
   }
 }
 
-
-//html5 for video the tag is video// 
+//html5 for video the tag is video//
